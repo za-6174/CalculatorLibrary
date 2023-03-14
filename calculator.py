39ef62d33ad04b5e33eadc5a221bb9dd64a0c549
Zainab Ali <zainab.z55.zz@gmail.com>
2023-03-14 18:09:51 +0300

    add division and multiplication methods

--- calculator.py
+++ calculator.py
@@ -7,3 +7,9 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+def division(first_term, second_term):
+    return first_term / second_term
+
+def multiplication(first_term, second_term):
+    return first_term * second_term
